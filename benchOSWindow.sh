1f4c93f3b8c194d9a026ad49fe6fdecd3b651eb3
Martín Dias <tinchodias@gmail.com>
2026-06-01 02:16:15 -0300

    improve

--- benchOSWindow.sh
+++ benchOSWindow.sh
@@ -9,6 +9,7 @@ BENCHES=(
 )
 
 for bench in "${BENCHES[@]}"; do
+    PHARO_WINDOW_DRIVER=OSSDL2Driver ./pharo-ui Pharo.image eval "OSSDL3GPURenderingBenchmark $bench andThen: [ Smalltalk snapshot: false andQuit: true ] future. #$bench"
     PHARO_WINDOW_DRIVER=OSSDL3Driver PHARO_WINDOW_RENDERER=OSSDL3FormRenderer ./pharo-ui Pharo.image eval "OSSDL3GPURenderingBenchmark $bench andThen: [ Smalltalk snapshot: false andQuit: true ] future. #$bench"
     PHARO_WINDOW_DRIVER=OSSDL3Driver PHARO_WINDOW_RENDERER=OSSDL3GPUFormRenderer ./pharo-ui Pharo.image eval "OSSDL3GPURenderingBenchmark $bench andThen: [ Smalltalk snapshot: false andQuit: true ] future. #$bench"
 done | tee -a results.txt
