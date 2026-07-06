1b038e1d38f8b689a0fed720bc9243e8e1658930
Martín Dias <tinchodias@gmail.com>
2026-07-06 12:28:34 -0400

    Update (fix) benchmark bash script

--- benchOSWindow.sh
+++ benchOSWindow.sh
@@ -7,9 +7,14 @@ BENCHES=(
     "benchTwoDistant"
     "benchVertical"
 )
+DRIVERS=(
+    "OSSDL2Driver"
+    "OSSDL3Driver"
+    "OSSDL3GPUDriver"
+)
 
 for bench in "${BENCHES[@]}"; do
-    PHARO_WINDOW_DRIVER=OSSDL2Driver ./pharo-ui Pharo.image eval "OSSDL3GPURenderingBenchmark $bench andThen: [ Smalltalk snapshot: false andQuit: true ] future. #$bench"
-    PHARO_WINDOW_DRIVER=OSSDL3Driver PHARO_WINDOW_RENDERER=OSSDL3FormRenderer ./pharo-ui Pharo.image eval "OSSDL3GPURenderingBenchmark $bench andThen: [ Smalltalk snapshot: false andQuit: true ] future. #$bench"
-    PHARO_WINDOW_DRIVER=OSSDL3Driver PHARO_WINDOW_RENDERER=OSSDL3GPUFormRenderer ./pharo-ui Pharo.image eval "OSSDL3GPURenderingBenchmark $bench andThen: [ Smalltalk snapshot: false andQuit: true ] future. #$bench"
+    for driver in "${DRIVERS[@]}"; do
+        PHARO_WINDOW_DRIVER=$driver ./pharo-ui Pharo.image eval "OSSDL3GPURenderingBenchmark $bench andThen: [ Smalltalk snapshot: false andQuit: true ] future. #$bench"
+    done
 done | tee -a results.txt
